4f36a130566ce0d4cf675f348be9f61395e2ac5f
Neil Bartlett <njbartlett@gmail.com>
2016-11-14 10:58:52 +0000

    EX: Added off-the-shelf components to basic webapp

--- org.example.webapp.bookstore/src/org/example/webapp/bookstore/BookstoreApp.java
+++ org.example.webapp.bookstore/src/org/example/webapp/bookstore/BookstoreApp.java
@@ -3,10 +3,14 @@ package org.example.webapp.bookstore;
 import org.osgi.service.component.annotations.Component;
 
 import com.effectiveosgi.annotation.RequireWebComponent;
+import osgi.enroute.polymer.app.webresource.capabilities.RequirePolymerAppWebresource;
+import osgi.enroute.polymer.iron.webresource.capabilities.RequirePolymerIronWebresource;
 import osgi.enroute.twitter.bootstrap.capabilities.RequireBootstrapWebResource;
 import osgi.enroute.webcomponentsjs.webresource.capabilities.RequireWebcomponentsJSWebresource;
 import osgi.enroute.webserver.capabilities.RequireWebServerExtender;
 
+@RequirePolymerIronWebresource(resource = "*.html")
+@RequirePolymerAppWebresource(resource = "*.html")
 @RequireWebComponent(name = "cart-list")
 @RequireWebcomponentsJSWebresource(resource = "webcomponents-lite.min.js")
 @RequireBootstrapWebResource(resource="css/bootstrap.css")
