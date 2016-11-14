dd5b03526c8483e052f5576a9f09f423cd18c67f
Neil Bartlett <njbartlett@gmail.com>
2016-11-14 04:53:31 +0000

    EX: Add vanilla list to app

--- org.example.webapp.bookstore/src/org/example/webapp/bookstore/BookstoreApp.java
+++ org.example.webapp.bookstore/src/org/example/webapp/bookstore/BookstoreApp.java
@@ -2,10 +2,12 @@ package org.example.webapp.bookstore;
 
 import org.osgi.service.component.annotations.Component;
 
+import com.effectiveosgi.annotation.RequireWebComponent;
 import osgi.enroute.twitter.bootstrap.capabilities.RequireBootstrapWebResource;
 import osgi.enroute.webcomponentsjs.webresource.capabilities.RequireWebcomponentsJSWebresource;
 import osgi.enroute.webserver.capabilities.RequireWebServerExtender;
 
+@RequireWebComponent(name = "cart-list")
 @RequireWebcomponentsJSWebresource(resource = "webcomponents-lite.min.js")
 @RequireBootstrapWebResource(resource="css/bootstrap.css")
 @RequireWebServerExtender
